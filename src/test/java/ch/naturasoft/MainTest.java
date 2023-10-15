7002a524158608c65ddf313137db8c573f92cb7f
urs <urs.frei@naturasoft.ch>
2023-10-15 12:16:52 +0200

    Hello Wordl

---
+++ src/test/java/ch/naturasoft/MainTest.java
@@ -0,0 +1,18 @@
+package ch.naturasoft;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testOk() {
+        assertTrue(true);
+    }
+
+    @Test
+    void testError() {
+        assertTrue(false);
+    }
+}
